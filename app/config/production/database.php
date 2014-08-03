30a3b9bd4415d2964df7b016140602cb1e9e9ba6
Kiumars Shahbazi <kshahbazi@fas.harvard.edu>
2014-08-02 22:44:17 -0400

    Update /production/database.php with new host

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -22,7 +22,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'mybuildings',
 			'username'  => 'robbyn',
 			'password'  => 'lyfi1Lz0',
